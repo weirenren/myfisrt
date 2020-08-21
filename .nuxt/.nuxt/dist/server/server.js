66fff5e1903890bf7e7a4563cab8d41510cc34b2
chao.wei <chao.wei@grabtaxi.com>
2020-08-21 23:46:12 +0800

    update

--- .nuxt/.nuxt/dist/server/server.js
+++ .nuxt/.nuxt/dist/server/server.js
@@ -104,7 +104,7 @@ module.exports =
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "/dist/";
+/******/ 	__webpack_require__.p = "/_nuxt/";
 /******/
 /******/ 	// uncaught error handler for webpack runtime
 /******/ 	__webpack_require__.oe = function(err) {
