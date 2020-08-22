89143379dc2ee3f4b7277ccde40ca03fb8d18b61
chao.wei <chao.wei@grabtaxi.com>
2020-08-22 13:55:15 +0800

    update

--- .nuxt/.nuxt/middleware.js
+++
@@ -1,9 +0,0 @@
-const middleware = {}
-
-middleware['authenticated'] = require('../middleware/authenticated.js')
-middleware['authenticated'] = middleware['authenticated'].default || middleware['authenticated']
-
-middleware['main'] = require('../middleware/main.js')
-middleware['main'] = middleware['main'].default || middleware['main']
-
-export default middleware
